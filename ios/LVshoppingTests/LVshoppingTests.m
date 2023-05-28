6f48eb2e2b59aab4052731c93f2b1646a669d6c8
MARYAM <seyedehmaryam.zahraei@mail.polimi.it>
2023-05-28 14:36:57 +0200

    about me edited

--- ios/LVshoppingTests/LVshoppingTests.m
+++ ios/LVshoppingTests/LVshoppingTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactApp2Tests : XCTestCase
+@interface LVshoppingTests : XCTestCase
 
 @end
 
-@implementation ReactApp2Tests
+@implementation LVshoppingTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
